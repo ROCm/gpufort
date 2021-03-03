5bc3ca8a04039fc3e7477a083e33fc41d51fd55b
Dominic Etienne Charrier <docharri@amd.com>
2021-03-03 13:51:09 +0000

    WiP: One more reduction issue fixed.

--- python/fort2hip/fort2hip.py
+++ python/fort2hip/fort2hip.py
@@ -13,11 +13,15 @@ import scanner.scanner as scanner
 fort2hipDir = os.path.dirname(__file__)
 exec(open("{0}/fort2hip_options.py.in".format(fort2hipDir)).read())
 
+# must be synced with grammar
+CLAUSE_NOT_FOUND           = -2
+CLAUSE_VALUE_NOT_SPECIFIED = -1
+
 def convertDim3(dim3,dimensions,doFilter=True):
      result = []
      specified = dim3
      if doFilter:
-         specified = [ x for x in dim3 if x != "-1" ]
+         specified = [ x for x in dim3 if x not in [str(CLAUSE_NOT_FOUND),str(CLAUSE_VALUE_NOT_SPECIFIED)]]
      for i,value in enumerate(specified):
           if i >= dimensions:
               break
@@ -202,7 +206,7 @@ def extractLoopKernels(loopKernels,index,cContext,fContext):
         # treat reduction vars / acc default(present) vars
         cContext["haveReductions"] = False # |= len(reductionOps)
         kernelCallArgNames = []
-        reductions    = kernelParseResult.gangTeamReduction(translator.makeCStr)
+        reductions    = kernelParseResult.gangTeamReductions(translator.makeCStr)
         reduction = []
         for arg in kernelArgs:
             name  = arg["name"]

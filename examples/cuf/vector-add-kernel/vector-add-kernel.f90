0ed1eb07ba3e64224553deba59f9317dba02ca9d
Dominic Etienne Charrier <dominic.charrier@googlemail.com>
2021-05-04 21:07:47 +0200

    CUF: Start working on kernel subroutines
    * Host code looks alright already.
    * HIP C++ kernel generation is still
      deactivated

---
+++ examples/cuf/vector-add-kernel/vector-add-kernel.f90
@@ -0,0 +1,43 @@
+program testSaxpy
+  use cudafor
+  implicit none
+  integer, parameter :: N = 40000
+  real :: x(N), y(N), a
+  real, device, allocatable :: x_d(:)
+  real, allocatable :: y_d(:)
+  !real, device :: x_d(N), y_d(N) ! fixed-size arrays are not fully 
+                       ! supported yet as alloc/dealloc are not generated yet
+  type(dim3) :: grid, tBlock
+  integer :: i
+
+  attributes(device) :: y_d
+
+  tBlock = dim3(256,1,1)
+  grid = dim3(ceiling(real(N)/tBlock%x),1,1)
+
+  allocate(x_d(N),y_d(N))
+
+  x = 1.0; y = 2.0; a = 2.0
+  x_d = x
+  y_d = y
+  
+  call gpuKernel<<<grid, tBlock>>>(a,x_d,y_d,N)
+
+  y = y_d
+
+  deallocate(x_d,y_d)
+
+  write(*,*) 'Max error: ', maxval(abs(y-4.0))
+
+contains 
+
+   attributes(global) subroutine gpuKernel(a,x,y,N)
+    real :: x(N), y(N), a
+    integer :: N
+    integer :: i
+    i = treadidx%x
+    if (i < N) then
+      y_d(i) = y_d(i) + a*x_d(i)
+    endif
+   end subroutine
+end program testSaxpy

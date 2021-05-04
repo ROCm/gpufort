b74a7352d45331e8bb70fc30adf6933fab15caba
Dominic Etienne Charrier <dominic.charrier@googlemail.com>
2021-05-04 10:07:29 +0200

    CUF example: Change to allocatable device arrays.

--- examples/cuf/vector-add/vector-add.f90
+++ examples/cuf/vector-add/vector-add.f90
@@ -3,12 +3,16 @@ program testSaxpy
   implicit none
   integer, parameter :: N = 40000
   real :: x(N), y(N), a
-  real, device :: x_d(N), y_d(N)
+  real, device, allocatable :: x_d(:), y_d(:)
+  !real, device :: x_d(N), y_d(N) ! fixed-size arrays are not fully 
+                       ! supported yet as alloc/dealloc are not generated yet
   type(dim3) :: grid, tBlock
   integer :: i
 
   tBlock = dim3(256,1,1)
   grid = dim3(ceiling(real(N)/tBlock%x),1,1)
+
+  allocate(x_d(N),y_d(N))
 
   x = 1.0; y = 2.0; a = 2.0
   x_d = x
@@ -20,5 +24,8 @@ program testSaxpy
   end do
 
   y = y_d
+
+  deallocate(x_d,y_d)
+
   write(*,*) 'Max error: ', maxval(abs(y-4.0))
 end program testSaxp

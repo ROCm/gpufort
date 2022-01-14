521e4daef2fdbc089b5c432c1f21426db9130b5a
Dominic Etienne Charrier <docharri@amd.com>
2022-01-14 05:00:32 -0500

    WiP: Move gpufort sources codegen from fort2x.hip in fort2x folder

--- python/fort2x/hip/render.py
+++ python/fort2x/hip/render.py
@@ -8,42 +8,7 @@ import jinja2
 import addtoplevelpath
 import utils.logging
         
-LOADER    = jinja2.FileSystemLoader(os.path.realpath(os.path.join(os.path.dirname(__file__),"templates")))
-ENV       = jinja2.Environment(loader=LOADER, trim_blocks=True, lstrip_blocks=True, undefined=jinja2.StrictUndefined)
-TEMPLATES = {}
-    
-def generate_code(template_path,context={}):
-    global ENV
-    global TEMPLATES
-    if template_path in TEMPLATES:
-        template = TEMPLATES[template_path]
-    else:
-        template = ENV.get_template(template_path)
-        TEMPLATES[template_path] = template
-    try:
-        return template.render(context)
-    except Exception as e:
-        utils.logging.log_error("fort2x.hip.render","generate_code","could not render template '%s'" % template_path)
-        raise e
-
-def generate_file(output_path,template_path,context={}):
-    with open(output_path, "w") as output:
-        output.write(generate_code(template_path,context))
-    
-def render_gpufort_header_file(output_path,context={}):
-    generate_file(output_path,"gpufort.template.h",context)
-
-def render_gpufort_reduction_header_file(output_path,context={}):
-    generate_file(output_path,"gpufort_reduction.template.h",context)
-
-def render_gpufort_array_header_file(output_path,context={}):
-    generate_file(output_path,"gpufort_array.template.h",context)
-
-def render_gpufort_array_source_file(output_path,context={}):
-    generate_file(output_path,"gpufort_array.template.cpp",context)
-
-def render_gpufort_array_fortran_interfaces_file(context={}):
-    generate_file(output_path,"gpufort_array.template.f03",context)
+from fort2x.render import *
 
 fort2hip_dir = os.path.dirname(__file__)
 exec(open(os.path.join(fort2hip_dir,"templates","render.py.in")).read())
---
+++ python/fort2x/render.py
@@ -0,0 +1,46 @@
+# SPDX-License-Identifier: MIT                                                
+# Copyright (c) 2021 Advanced Micro Devices, Inc. All rights reserved.
+import os
+import pprint
+
+import jinja2
+
+import addtoplevelpath
+import utils.logging
+        
+LOADER    = jinja2.FileSystemLoader(os.path.realpath(os.path.join(os.path.dirname(__file__),"templates")))
+ENV       = jinja2.Environment(loader=LOADER, trim_blocks=True, lstrip_blocks=True, undefined=jinja2.StrictUndefined)
+TEMPLATES = {}
+    
+def generate_code(template_path,context={}):
+    global ENV
+    global TEMPLATES
+    if template_path in TEMPLATES:
+        template = TEMPLATES[template_path]
+    else:
+        template = ENV.get_template(template_path)
+        TEMPLATES[template_path] = template
+    try:
+        return template.render(context)
+    except Exception as e:
+        utils.logging.log_error("fort2x.hip.render","generate_code","could not render template '%s'" % template_path)
+        raise e
+
+def generate_file(output_path,template_path,context={}):
+    with open(output_path, "w") as output:
+        output.write(generate_code(template_path,context))
+    
+def render_gpufort_header_file(output_path,context={}):
+    generate_file(output_path,"gpufort.template.h",context)
+
+def render_gpufort_reduction_header_file(output_path,context={}):
+    generate_file(output_path,"gpufort_reduction.template.h",context)
+
+def render_gpufort_array_header_file(output_path,context={}):
+    generate_file(output_path,"gpufort_array.template.h",context)
+
+def render_gpufort_array_source_file(output_path,context={}):
+    generate_file(output_path,"gpufort_array.template.cpp",context)
+
+def render_gpufort_array_fortran_interfaces_file(context={}):
+    generate_file(output_path,"gpufort_array.template.f03",context)

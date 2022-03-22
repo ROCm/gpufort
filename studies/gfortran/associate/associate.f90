2d41caa642c2d6cb2aa246fb4cd42ddfbf07c07f
Dominic Etienne Charrier <docharri@amd.com>
2022-03-22 16:18:30 -0400

    (WiP) Modified study to model host_data with if and if_present

--- studies/gfortran/associate/associate.f90
+++ studies/gfortran/associate/associate.f90
@@ -3,14 +3,21 @@
 program main
   integer :: x = 5
   integer :: b = 3
+  logical :: condition = .true.
 
-  associate (x => myfunc(b))
+  associate (x => myfunc(x,b,condition))
     print *, x
   end associate
 
 contains
-  function myfunc(i)
-    integer :: i, myfunc
-    myfunc = i*2
+  function myfunc(x,b,condition)
+    integer,target :: x,b
+    logical :: condition
+    integer,pointer :: myfunc
+    if ( condition ) then
+      myfunc => x
+    else 
+      myfunc => b
+    endif
   end function
 end program

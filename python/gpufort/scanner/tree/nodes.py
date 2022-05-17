555881c8235483fa8132259d703b100ada65746a
Dominic Etienne Charrier <dominic.charrier@googlemail.com>
2022-05-17 15:03:05 +0200

    (WiP)Fix: /= 0 translation: ignore symbols that are not variables

--- python/gpufort/scanner/tree/nodes.py
+++ python/gpufort/scanner/tree/nodes.py
@@ -825,13 +825,16 @@ class STNonZeroCheck(STNode):
                 result):
             parse_result = tokens[0]
             lhs_name = parse_result.lhs_f_str()
-            ivar  = indexer.scope.search_index_for_var(index,self.parent.tag(),\
-              lhs_name)
-            on_device = index_var_is_on_device(ivar)
-            transformed |= on_device
-            if on_device:
-                subst = parse_result.f_str() # TODO backend specific
-                result = result.replace(result[start:end], subst)
+            try:
+                ivar  = indexer.scope.search_index_for_var(index,self.parent.tag(),\
+                  lhs_name)
+                on_device = index_var_is_on_device(ivar)
+                transformed |= on_device
+                if on_device:
+                    subst = parse_result.f_str() # TODO backend specific
+                    result = result.replace(result[start:end], subst)
+            except: # do not care if this is not a variable
+                pass
         return result, transformed
 
 
--- test/stability/test0017.f90
+++ test/stability/test0017.f90
@@ -1,10 +1,12 @@
 program main
   implicit none
   integer i,n
+  character(len=*) :: a
 
   do
     read(unit,*,END=9)
     i=i+1
+    if ( index(a,'b') /= 0 ) i = i*2
   end do
 9 n = i/4
 end program main

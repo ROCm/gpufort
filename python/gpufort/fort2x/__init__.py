744f7898428dfd030b5b1bec3cab2afdd4164bba
Dominic Etienne Charrier <docharri@amd.com>
2022-05-23 20:37:50 +0200

    (WiP) Remove debug output

--- python/gpufort/fort2x/__init__.py
+++ python/gpufort/fort2x/__init__.py
@@ -16,5 +16,3 @@ from . import render
 
 #from . import opts # might not work; might create new opts module that differs from the package-local opts module
 from .render import opts # imports package-local opts module
-
-print("init gpufort.fort2x")
